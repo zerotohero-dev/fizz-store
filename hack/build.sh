607985b1e65cf359eb111fb7fac25308960dda60
Volkan Ozcelik <me@volkan.io>
2021-07-17 14:02:00 -0700

    lastworking

--- hack/build.sh
+++ hack/build.sh
@@ -2,11 +2,39 @@
 
 #  \
 #  \\,
-#   \\\,^,.,,.                    “Zero to Hero”
+#   \\\,^,.,,.                     Zero to Hero
 #   ,;7~((\))`;;,,               <zerotohero.dev>
 #   ,(@') ;)`))\;;',    stay up to date, be curious: learn
 #    )  . ),((  ))\;,
 #   /;`,,/7),)) )) )\,,
 #  (& )`   (,((,((;( ))\,
 
-docker build -t zerotohero-dev/fizz-store:0.0.10 .
+IMAGE=$ECR_IMAGE_FIZZ_STORE
+TAG=$ECR_TAG_FIZZ_STORE
+REPO=$ECR_REPO 
+
+echo "»»» building"
+docker build -t $IMAGE:$TAG .
+retVal=$?
+if [ $retVal -ne 0 ]; then
+  echo "Error building the image."
+  exit 1
+fi
+
+echo "»»» tagging"
+docker tag $IMAGE:$TAG $REPO/$IMAGE:$TAG
+retVal=$?
+if [ $retVal -ne 0 ]; then
+  echo "Error tagging the image."
+  exit 1
+fi
+
+echo "»»» pushing"
+docker push $REPO/$IMAGE:$TAG
+retVal=$?
+if [ $retVal -ne 0 ]; then
+  echo "Error pushing the image."
+  exit 1
+fi
+
+echo "»»» Everything is awesome! «««"
